464ddb2f7d80770c3252b05dc3f9b35d9ec9f31d
SeifRashedd <seifrashedswe@gmail.com>
2024-05-07 12:44:13 +0300

    first commet

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.black,
       ),
     );
   }
